66e576b3f6f39d3269d980cd76130d2821273454
Sean Gibson <seanthomasgibson@gmail.com>
2021-03-18 13:02:20 -0700

    Refactored function components to classes. Modified Joke in src/Joke.js & JokeList in src/JokeList.js.

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,39 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
+  }
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  upVote() {
+    this.props.vote(this.props.id, +1);
+  }
+  downVote() {
+    this.props.vote(this.props.id, -1);
+  }
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
-
-        {votes}
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+  
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+  
+          {this.props.votes}
+        </div>
+  
+        <div className="Joke-text">{this.props.text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Joke;
